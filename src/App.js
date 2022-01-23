99cb3149b02c8c08b0481b2b09cec20dd9a3b121
Jennfier Heejin Kang <39927897+HKang0302@users.noreply.github.com>
2022-01-23 23:10:23 +0900

    working on home - need to add text without effect of opacity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Nav } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import styled from 'styled-components';
+
+import homeImg from './Home.jpg'
+
 
 function App() {
+  var [isNotFaded, setIsNotFaded] = useState(true);
+  
+  useEffect(()=>{ 
+    setIsNotFaded(true);
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav className="justify-content-center" activeKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">HOME</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">ABOUT</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">PROJECTS</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled">CONTACT</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      <CSSTransition in={isNotFaded} classNames="fade" timeout={1000}>
+        <div className='home-img'>
+          <div className='home-text'>
+            <p>println(“Hello World!”);</p>
+            <p>My name is Heejin or simply Jennifer :) </p>
+          </div>
+        </div>
+      </CSSTransition>
+      <div>
+        hello
+      </div>
     </div>
   );
 }
